5abbb87e0cbb895982de0d9efc9f66dc5395082f
Backend <55504202+Backend2121@users.noreply.github.com>
2025-10-25 19:29:06 +0200

    Add files via upload

---
+++ W_clock.py
@@ -0,0 +1,25 @@
+import time
+from display import getOled
+from utils import centerString
+from utils import settime
+from utils import ActiveWidget
+
+VERSION = 100
+
+widget_number = 0
+
+def run(id, active_widget):
+    global widget_number
+    oled = getOled()
+    settime()
+    while True:
+        time.sleep(0.2)
+        if (active_widget.value == widget_number):
+            (year, month, mday, hour, minute, second, weekday, yearday) = time.gmtime()
+            dateString = f'{mday}/{month}/{year}'
+            timeString = '{:02d}:{:02d}:{:02d}'.format(hour, minute, second)
+            oled.fill(0)
+            oled.text("Data e ora", centerString("Data e ora"), 0)
+            oled.text(dateString, centerString(dateString), 16)
+            oled.text(timeString, centerString(timeString), 32)
+            oled.show()
